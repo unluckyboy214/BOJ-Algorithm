c1525d7925918d938c9db26134467edeb92984f6
unluckyboy214 <141120169+unluckyboy214@users.noreply.github.com>
2024-04-12 00:01:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,55 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 #include <cstring>
+#include <algorithm>
+
 using namespace std;
-
-vector<int> a[1001];
+vector<int> v[1001];
 bool check[1001];
-int N, M, V, x, y;
 
-void dfs(int V) {
-    check[V] = true;
-    cout << V << " ";
-    for (auto& next : a[V]) {
-        if (!check[next]) dfs(next);
-    }
+void dfs(int start) {
+	check[start] = true;
+	cout << start << " ";
+	for (auto& next : v[start]) {
+		if (!check[next]) dfs(next);
+	}
 }
-void bfs(int V) {
-    queue<int> q;
-    q.push(V);
-    check[V] = true;
-    while (!q.empty()){
-        int cur = q.front(); 
-        q.pop();
-        cout << cur << " ";
-        for (auto& next : a[cur]) {
-            if (!check[next]) {
-                check[next] = true;
-                q.push(next);
-            }
-        }
-    }
+
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	check[start] = true;
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+		cout << now << " ";
+		for (auto& next : v[now]) {
+			if (!check[next]) {
+				check[next] = true;
+				q.push(next);
+			}
+		}
+	}
 }
- 
+
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-    cin >> N >> M >> V;
-    for (int i = 0; i < M; i++) {
-        cin >> x >> y;
-        a[x].push_back(y);
-        a[y].push_back(x);
-    }
-    for (auto& list : a) {
-        sort(list.begin(), list.end());
-    }
-    dfs(V);
-    cout << "\n";
-    memset(check, false, sizeof(check));
-    bfs(V);
+	int N, M, V, x, y;
+	cin >> N >> M >> V;
 
-    return 0;
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	for (auto& list : v) {
+		sort(list.begin(), list.end());
+	}
+	dfs(V);
+	cout << "\n";
+	memset(check, false, sizeof(check));
+	bfs(V);
+
+
+	return 0;
 }
