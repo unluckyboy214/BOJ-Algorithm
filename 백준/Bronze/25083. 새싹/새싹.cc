49701dc9778f8a5ff3caa135bdeb8b341a55ce05
unluckyboy214 <141120169+unluckyboy214@users.noreply.github.com>
2024-06-25 15:59:23 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie();
+
+	cout << "         ,r\'\"7" << "\n";
+	cout << "r`-_   ,\'  ,/" << "\n";
+	cout << " \\. \". L_r\'" << "\n";
+	cout << "   `~\\/" << "\n";
+	cout << "      |" << "\n";
+	cout << "      |" << "\n";
+
+	return 0;
+}
