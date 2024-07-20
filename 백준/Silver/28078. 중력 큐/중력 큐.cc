ababf9e07e5fe95a7e50d36ff7bdacc64f0adb0a
unluckyboy214 <141120169+unluckyboy214@users.noreply.github.com>
2024-07-20 22:57:52 +0900

    [Silver I] Title: 중력 큐, Time: 88 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/28078. 중력 큐/중력 큐.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+int Q, Dir, B, W;
+deque<char> DQ;
+string S;
+char C;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> Q;
+    while (Q--) {
+        cin >> S;
+        if (S == "push") {
+            cin >> C;
+            if ((Dir == 0) || (Dir == 2)) {
+                DQ.push_front(C);
+                if (C == 'b') {
+                    B++;
+                }
+                else if (C == 'w') {
+                    W++;
+                }
+            }
+            else if (Dir == 1) {
+                if (C == 'b') {
+                    if (!DQ.empty()) {
+                        if (DQ.back() == 'w') {
+                            B++;
+                            DQ.push_front(C);
+                        }
+                    }
+                }
+                else if (C == 'w') {
+                    W++;
+                    DQ.push_front(C);
+                }
+            }
+            else if (Dir == 3) {
+                if (C == 'w') {
+                    W++;
+                    DQ.push_front(C);
+                }
+            }
+        }
+        else if (S == "pop") {
+            if (!DQ.empty()) {
+                if (Dir == 1) {
+                    if (DQ.back() == 'w') {
+                        DQ.pop_back();
+                        W--;
+                        while (!DQ.empty()) {
+                            if (DQ.back() == 'b') {
+                                DQ.pop_back();
+                                B--;
+                            }
+                            else {
+                                break;
+                            }
+                        };
+                    }
+                }
+                else {
+                    if (DQ.back() == 'b') {
+                        B--;
+                    }
+                    else if (DQ.back() == 'w') {
+                        W--;
+                    }
+                    DQ.pop_back();
+                }
+            }
+        }
+        else if (S == "rotate") {
+            cin >> C;
+            if (C == 'l') {
+                Dir--;
+                if (Dir == -1) {
+                    Dir = 3;
+                }
+            }
+            else if (C == 'r') {
+                Dir++;
+                if (Dir == 4) {
+                    Dir = 0;
+                }
+            }
+            if (Dir == 1) {
+                while (!DQ.empty()) {
+                    if (DQ.back() == 'b') {
+                        DQ.pop_back();
+                        B--;
+                    }
+                    else {
+                        break;
+                    }
+                };
+            }
+            else if (Dir == 3) {
+                while (!DQ.empty()) {
+                    if (DQ.front() == 'b') {
+                        DQ.erase(DQ.begin());
+                        B--;
+                    }
+                    else {
+                        break;
+                    }
+                };
+            }
+        }
+        else if (S == "count") {
+            cin >> C;
+            if (C == 'b') {
+                cout << B << "\n";
+            }
+            else if (C == 'w') {
+                cout << W << "\n";
+            }
+        }
+    };
+
+    return 0;
+}
