374c2c6a38f47176b3aff0e0c8d8a554efaaad0f
unluckyboy214 <141120169+unluckyboy214@users.noreply.github.com>
2024-07-25 17:56:59 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int N;
+	cin >> N;
+
+	vector<int> dp(N + 1,0);
+
+	//dp[1]은 이미 0. 굳이 추가 연산 필요x
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+		if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+	}
+
+	cout << dp[N];
+
+	return 0;
+}
