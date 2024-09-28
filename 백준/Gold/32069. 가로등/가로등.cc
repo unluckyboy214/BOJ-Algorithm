926388623decc90f7c370f4e9ed130b0f9a5eaf1
unluckyboy214 <141120169+unluckyboy214@users.noreply.github.com>
2024-09-28 22:46:50 +0900

    [Gold V] Title: 가로등, Time: 356 ms, Memory: 62680 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.cc
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 #include <queue>
 #include <set>
 
@@ -17,23 +17,23 @@ bool isPossible(long long x) {
 
 int main() {
     // 도로 길이, 가로등 개수, 출력할 위치 개수를 입력 받는다
-    cin >> l >> n >> k;
+    scanf("%lld%d%d", &l, &n, &k);
 
     // 각 가로등의 위치를 입력 받고 큐에 추가, 방문 표시
     for (int i = 0; i < n; ++i) {
-        cin >> a;
-        q.push({ a, 0 }); // 가로등 위치에서의 어두운 정도는 0
-        visited.insert(a); // 가로등 위치를 방문했다고 표시
+        scanf("%lld", &a);
+        q.push({ a, 0 });
+        visited.insert(a);
     }
 
     // BFS 시작
     while (!q.empty()) {
-        long long here = q.front().first; // 현재 위치
-        long long dist = q.front().second; // 해당 위치의 어두운 정도 (가장 가까운 가로등까지의 거리)
+        long long here = q.front().first;
+        long long dist = q.front().second;
         q.pop();
 
-        // 현재 위치의 어두운 정도를 출력
-        cout << dist << endl;
+        // 현재 위치의 어두운 정도 (가장 가까운 가로등까지의 거리)를 출력
+        printf("%lld\n", dist);
         --k;
 
         // K개의 위치를 모두 출력했으면 프로그램 종료
@@ -46,9 +46,9 @@ int main() {
             long long there = here + dx[i];
 
             // 이웃 위치가 도로 범위 내에 있고 아직 방문하지 않았다면 큐에 추가
-            if (isPossible(there) && visited.find(there) == visited.end()) {
-                q.push({ there, dist + 1 }); // 거리는 이전보다 1 증가
-                visited.insert(there); // 방문 기록
+            if (isPossible(there) && !visited.count(there)) {
+                q.push({ there, dist + 1 });
+                visited.insert(there);
             }
         }
     }
